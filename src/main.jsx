b68b8019052dea2554af71f9b8183ed02a76bcb4
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-04-23 11:59:49 +0600

    assignment 07

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+import Navbar from './Components/Navbar/Navbar'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Navbar,
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
