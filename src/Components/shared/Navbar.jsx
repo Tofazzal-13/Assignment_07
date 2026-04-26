b6d734d539031501b16c451523a535c6cc0cc6c6
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-04-26 10:39:31 +0600

    navbar correction again

--- src/Components/shared/Navbar.jsx
+++ src/Components/shared/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <IoMenu className="text-2xl" />
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52">
                             {menuLinks}
                         </ul>
                     </div>
