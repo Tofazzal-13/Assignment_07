fef77295d0a64e1dbe6364d59056c9b287b34846
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-04-26 10:29:37 +0600

    navbar correction

--- src/Components/shared/Navbar.jsx
+++ src/Components/shared/Navbar.jsx
@@ -1,62 +1,61 @@
 import React from 'react';
-import { IoHomeOutline } from "react-icons/io5";
+import { IoHomeOutline, IoMenu } from "react-icons/io5";
 import { CiClock2 } from "react-icons/ci";
 import { FaChartLine } from "react-icons/fa";
-import { Link, NavLink } from 'react-router';
+import { Link } from 'react-router';
 import logoImg from "../../assets/images/logo.png"
 import MyNavLink from './MyNavLink';
 
-
-
-
 const Navbar = () => {
     const navItems = [
-        {
-            path: "/",
-            text: "Home",
-            icon: <IoHomeOutline />
-        },
-        {
-            path: "/timeline",
-            text: "Timeline",
-            icon: <CiClock2 />
-        },
-        {
-            path: "/stats",
-            text: "Stats",
-            icon: <FaChartLine />
-        },
+        { path: "/", text: "Home", icon: <IoHomeOutline /> },
+        { path: "/timeline", text: "Timeline", icon: <CiClock2 /> },
+        { path: "/stats", text: "Stats", icon: <FaChartLine /> },
+    ];
 
-    ]
+    const menuLinks = (
+        <>
+            {navItems.map((item, index) => (
+                <li key={index}>
+                    <MyNavLink to={item.path}>
+                        <div className='flex items-center gap-1'>
+                            {item.icon} {item.text}
+                        </div>
+                    </MyNavLink>
+                </li>
+            ))}
+        </>
+    );
+
     return (
-        <nav className="sticky top-0 z-50 bg-white/80  border-b border-gray-100">
-
-            <div className='container mx-auto'>
-                <div className="navbar">
-                    <div className="flex-1">
-                        <Link to="/">
-                            <img src={logoImg} alt=''></img>
-                        </Link>
-                    </div>
-                    <div className="flex-none">
-                        <ul className='flex gap-2'>
-                            {
-                                navItems.map((item, index) => <MyNavLink to={item.path} key={index}>
-                                    <div className='flex items-center gap-1'>
-                                        {item.icon} {item.text}
-                                    </div>
-                                </MyNavLink>)
-                            }
-                        </ul>
-
-                    </div>
+        <div className="sticky top-0 z-50 bg-white/80 border-b border-gray-100 backdrop-blur-md">
+            <div className="navbar container mx-auto">
+                {/* Logo Section */}
+                <div className="navbar-start">
+                    <Link to="/">
+                        <img src={logoImg} alt='Logo' className="h-10" />
+                    </Link>
                 </div>
 
+                {/* Menu Section */}
+                <div className="navbar-end">
+                    {/* Desktop Menu */}
+                    <ul className="menu menu-horizontal hidden lg:flex px-1 gap-2">
+                        {menuLinks}
+                    </ul>
 
-
+                    {/* Mobile Hamburger Menu */}
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <IoMenu className="text-2xl" />
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {menuLinks}
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </nav>
-
+        </div>
     );
 };
 
